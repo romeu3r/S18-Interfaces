dc2fe29049768a0bbf27950b36332f08b0445694
romeu3r <romeu3r@gmail.com>
2022-01-22 16:27:38 -0300

    Solução sem Interface!

---
+++ src/model/services/BrazilTaxService.java
@@ -0,0 +1,10 @@
+package model.services;
+
+public class BrazilTaxService {
+    public double tax(double amout) {
+        if (amout <= 100.0)
+            return amout * 0.2;
+        else
+            return amout * 0.15;
+    }
+}
