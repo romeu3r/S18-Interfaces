5f90ae8b9f7781d7d3153e8a6e0691e641903101
romeu3r <romeu3r@gmail.com>
2022-01-22 17:09:55 -0300

    Solução com interface.

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,10 +1,10 @@
 package model.services;
 
-public class BrazilTaxService {
-    public double tax(double amout) {
-        if (amout <= 100.0)
-            return amout * 0.2;
+public class BrazilTaxService implements TaxService {
+    public double tax(double amount) {
+        if (amount <= 100.0)
+            return amount * 0.2;
         else
-            return amout * 0.15;
+            return amount * 0.15;
     }
 }
